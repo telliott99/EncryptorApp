32169cfa1326d223e842c36c7eb4dc3e4b30786c
Tom Elliott <telliott@hsc.wvu.edu>
2015-12-13 21:09:04 -0500

    initial commit

--- Encryptor/Decrypt.swift
+++ Encryptor/Decrypt.swift
@@ -50,4 +50,29 @@ extension Encryptor {
         print("result:\n\(result)\n")
         return result
     }
+    
+    func decryptMany(input: BinaryData) -> BinaryData {
+        print("decryptMany")
+        var i = 0
+        var j = 0
+        let n = 16
+        var ret = BinaryData()
+        var tmp = BinaryData()
+        var currentIV = self.iv
+        
+        while i < input.count {
+            j = min(input.count, i + n)
+            tmp = self.decryptOneChunk(
+                input[i..<j],
+                iv: currentIV)
+            ret = ret + tmp
+            
+            // crucial change:
+            // currentIV = tmp
+            currentIV = input[i..<j]
+            i = i + n
+        }
+        return ret
+    }
+
 }
--- Encryptor/Encrypt.swift
+++ Encryptor/Encrypt.swift
@@ -63,4 +63,27 @@ extension Encryptor {
         print("result:\n\(result)\n")
         return result
     }
+    
+    func encryptMany(input: BinaryData) -> BinaryData {
+        print("encryptMany")
+        var i = 0
+        var j = 0
+        let n = 16
+        var ret = BinaryData()
+        var tmp = BinaryData()
+        var currentIV = self.iv
+        
+        while i < input.count {
+            j = min(input.count, i + n)
+            print("encrypt round: \(i/16 + 1)")
+            tmp = self.encryptOneChunk(
+                input[i..<j],
+                iv: currentIV)
+            ret = ret + tmp
+            currentIV = tmp
+            i = i + n
+        }
+        return ret
+    }
+
 }
